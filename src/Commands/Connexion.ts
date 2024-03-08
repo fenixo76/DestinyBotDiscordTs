ad4c39713efa20a853b23465847055ba1cf570ef
fenixo76 <57808345+fenixo76@users.noreply.github.com>
2024-03-08 11:00:22 +0100

    Update Connexion.ts

--- src/Commands/Connexion.ts
+++ src/Commands/Connexion.ts
@@ -12,7 +12,7 @@ export const command: SlashCommand = {
             .setColor("#FF00D8")
             .setTitle("Connexion")
             .setThumbnail(`https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}`)
-            .setDescription(`Connecte-toi au site : [Orphidia](https://www.bungie.net/en/OAuth/Authorize?response_type=code&client_id=${process.env.BUNGIE_OAUTH_CLIENT_ID}) Pour avoir accès aux commandes Destiny du serveur.\n\nVous avez 30 Seconde pour vous connecté`)
+            .setDescription(`Connecte-toi au site : [NOM_DE_VOTRE_SITE](https://www.bungie.net/en/OAuth/Authorize?response_type=code&client_id=${process.env.BUNGIE_OAUTH_CLIENT_ID}) Pour avoir accès aux commandes Destiny du serveur.\n\nVous avez 30 Seconde pour vous connecté`)
 
         interaction.reply({ embeds: [embed], fetchReply: true, ephemeral: true })
     }
